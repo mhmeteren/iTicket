b3f809f93b8005086544ed72c22b5b77386cee3e
mhmeteren <mehmeter3n@gmail.com>
2024-05-26 17:55:50 +0300

    feat: Added Base Request class for paging

---
+++ Core/iTicket.Application/Bases/BaseRequestForPaging.cs
@@ -0,0 +1,21 @@
+﻿namespace iTicket.Application.Bases
+{
+    public class BaseRequestForPaging
+    {
+        private int _currentPage = 1;
+
+        public int CurrentPage
+        {
+            get { return _currentPage; }
+            set { _currentPage = value > 0 ? value : _currentPage; }
+        }
+
+        private int _pagesize = 5;
+
+        public int PageSize
+        {
+            get { return _pagesize; }
+            set { _pagesize = value is > 0 and <= 20 ? value : _pagesize; }
+        }
+    }
+}
--- Core/iTicket.Application/Features/Companies/Queries/GetAllCompaniesByPaging/GetAllCompaniesByPagingQueryRequest.cs
+++ Core/iTicket.Application/Features/Companies/Queries/GetAllCompaniesByPaging/GetAllCompaniesByPagingQueryRequest.cs
@@ -1,24 +1,11 @@
-﻿using MediatR;
+﻿using iTicket.Application.Bases;
+using MediatR;
 
 namespace iTicket.Application.Features.Companies.Queries.GetAllCompaniesByPaging
 {
-    public class GetAllCompaniesByPagingQueryRequest: IRequest<IList<GetAllCompaniesByPagingQueryResponse>>
+    public class GetAllCompaniesByPagingQueryRequest: BaseRequestForPaging, IRequest<IList<GetAllCompaniesByPagingQueryResponse>>
     {
-        private int _currentPage = 1;
 
-        public int CurrentPage
-        {
-            get { return _currentPage; }
-            set { _currentPage = value > 0 ? value : _currentPage; }
-        }
-
-        private int _pagesize = 5;
-
-        public int PageSize
-        {
-            get { return _pagesize; }
-            set { _pagesize = value is > 0 and <= 20 ? value : _pagesize; }
-        }
     }
 
 }
