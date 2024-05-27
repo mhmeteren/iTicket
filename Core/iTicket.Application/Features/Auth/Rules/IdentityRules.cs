bde8b125113e99f6afab3f48d35bb75ff0e7a52f
mhmeteren <mehmeter3n@gmail.com>
2024-05-27 17:18:53 +0300

    Added custom exception class for IdentityResult and integrated it into ExceptionMiddleware.

---
+++ Core/iTicket.Application/Features/Auth/Rules/IdentityRules.cs
@@ -0,0 +1,16 @@
+﻿using iTicket.Application.Bases;
+using iTicket.Application.Features.Auth.Exceptions;
+using Microsoft.AspNetCore.Identity;
+
+namespace iTicket.Application.Features.Auth.Rules
+{
+    public class IdentityRules : BaseRules
+    {
+        public Task IdentityResultValidation(IdentityResult result)
+        {
+            if (!result.Succeeded)
+                throw new IdentityResultException(result);
+            return Task.CompletedTask;
+        }
+    }
+}
