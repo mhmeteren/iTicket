be6ad60dfeb97f17fc7f82b59df1b762edae9991
mhmeteren <mehmeter3n@gmail.com>
2024-06-03 15:46:48 +0300

    Added route search functionality for users.

---
+++ Core/iTicket.Application/Features/BusSchedules/Queries/GetAllBusSchedulesByRoutesAndPaging/GetAllBusSchedulesByRoutesAndPagingQueryRequest.cs
@@ -0,0 +1,13 @@
+﻿using iTicket.Application.Bases;
+using MediatR;
+
+namespace iTicket.Application.Features.BusSchedules.Queries.GetAllBusSchedulesByRoutesAndPaging
+{
+    public class GetAllBusSchedulesByRoutesAndPagingQueryRequest : BaseRequestForPaging, IRequest<IList<GetAllBusSchedulesByRoutesAndPagingQueryResponse>>
+    {
+        public int StartBusStationId { get; set; } = 0;
+        public int EndBusStationId { get; set; } = 0;
+        public int TripType { get; set; } = (int)Common.Enums.TripType.TwoPlusOne;
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
